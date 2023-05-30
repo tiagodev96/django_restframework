ae13aae7430374820f8d2708a350cebb13f0cf04
Tiago Batista <tiago.dev96@gmail.com>
2023-05-30 06:20:17 -0300

    :tada: first commit

---
+++ setup/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+from school.views import students
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('students/', students)
+]
