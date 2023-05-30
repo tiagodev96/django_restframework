79bf85f7cf913062b97a7cd8584fd26930e30036
Tiago Batista <tiago.dev96@gmail.com>
2023-05-30 06:40:11 -0300

    Adding models and db migrations

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from school.models import Student, Course
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+    list_display = ('id', 'name', 'document', 'birth_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 20
+  
+admin.site.register(Student, Students)
+
+class Courses(admin.ModelAdmin):
+    list_display = ('id', 'course_code', 'description', 'level')
+    list_display_links = ('id', 'course_code')
+    search_field = ('course_code')
+
+admin.site.register(Course, Courses)
--- setup/settings.py
+++ setup/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'school',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
